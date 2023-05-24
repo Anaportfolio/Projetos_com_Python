d7300f7d07b000d03f291e09cf2a8ab6674c8851
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-05-24 11:07:45 -0300

    interface

--- Interface/interface.py
+++ Interface/interface.py
@@ -1,8 +1,8 @@
 # Importando a Biblioteca 
-import tkinter 
+from tkinter import*
 
 # Criando a janela 
-janela = tkinter.Tk()
+janela = Tk()
 
 # Tamanho da janela 
 janela.geometry("300x300")
@@ -12,9 +12,10 @@ janela.title("Pimeira Interface com Python")
 
 
 # Colocando um texto dentro da janela 
-texto = tkinter.Label(janela, text="Olá mundo!")
+texto =Label(janela, text="Olá mundo!")
 # Exibindo o texto na janela 
 texto.pack()
+
 
 # Exibindo a tela 
 janela.mainloop()
